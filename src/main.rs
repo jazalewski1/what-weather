77c745731b14a7b7b466d0721baa55745f8fcea4
Zalewski Jakub <jazalewski1@gmail.com>
2025-08-11 23:12:24 +0200

    Run driver

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-use what_weather::controller::adapters;
-use what_weather::domain::model;
-use what_weather::domain::model::Reporter;
-
 fn main() {
-    let reporter = model::WeatherReporter::new(
-        Box::new(adapters::FakeGeolocationProvider),
-        Box::new(adapters::FakeWeatherProvider),
-        Box::new(adapters::ConsolePresenter),
-    );
-    reporter.fetch_and_report(&model::Parameters);
+    what_weather::controller::driver::run();
 }
