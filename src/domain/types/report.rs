14d12d9ca77f9252d53e5945ec7cc10b3abe2711
Jakub Zalewski <52531410+jazalewski1@users.noreply.github.com>
2025-08-12 19:59:56 +0200

    feat: Introduce weather kinds (#5)

--- src/domain/types/report.rs
+++ src/domain/types/report.rs
@@ -1,12 +1,55 @@
 use crate::domain::types::Coordinates;
 
-#[derive(Debug, PartialEq, Eq)]
-pub enum WeatherKind {
-    Sunny,
-    Rain,
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum Clouds {
+    Clear,
+    Light,
+    Moderate,
+    Dense,
 }
 
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum Fog {
+    Normal,
+    Rime,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum PrecipitationKind {
+    Rain,
+    Snow,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum PrecipitationIntensity {
+    Light,
+    Moderate,
+    Heavy,
+    Shower,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum PrecipitationHeat {
+    Freezing,
+    Normal,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct Precipitation {
+    pub kind: PrecipitationKind,
+    pub intensity: PrecipitationIntensity,
+    pub heat: PrecipitationHeat,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum WeatherKind {
+    Clouds(Clouds),
+    Fog(Fog),
+    Precipitation(Precipitation),
+    Thunderstorm,
+}
+
+#[derive(Copy, Clone, Debug)]
 pub struct WeatherReport {
     pub coordinates: Coordinates,
     pub kind: WeatherKind,
