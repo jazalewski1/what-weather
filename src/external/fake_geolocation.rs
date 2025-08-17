747a9058cb4163f4056b7a29c80c9719b31f6cc9
Jakub Zalewski <52531410+jazalewski1@users.noreply.github.com>
2025-08-17 22:37:54 +0200

    feat: Specify coordinates from CLI (#17)

--- src/external/fake_geolocation.rs
+++ src/external/fake_geolocation.rs
@@ -6,8 +6,8 @@ pub struct FakeGeolocationProvider;
 impl GeolocationProvider for FakeGeolocationProvider {
     fn get_current_coordinates(&self) -> Coordinates {
         Coordinates {
-            latitude: 51.106941500927306,
-            longitude: 17.077312228247923,
+            latitude: 51.10694,
+            longitude: 17.07731,
         }
     }
 }
